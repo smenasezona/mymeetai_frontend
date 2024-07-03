dac7be473823b75dfbba18ff5c94c976aa2a6e94
smenasezona <artemkiasko@gmail.com>
2024-07-04 01:17:27 +0300

    add pretty border

--- app/components/BookItem.tsx
+++ app/components/BookItem.tsx
@@ -7,23 +7,39 @@ interface BookItemProps {
 
 const BookItem: React.FC<BookItemProps> = ({book}) => {
     return (
-        <div className="border-2 border-amber-400 rounded-lg shadow-md overflow-hidden flex w-full">
-            <div className="flex-shrink-0">
-                <picture>
-                    <img
-                        width={250}
-                        height={500}
-                        src={book.cover}
-                        alt={book.title}
-                    />
-                </picture>
+        <div className="relative w-full p-1 border-2 border-amber-400">
+            <div
+                className="absolute top-1 left-1 w-3 h-3 border-2 border-amber-400 text-amber-400 flex items-center justify-center text-xs">•
             </div>
-            <div className="pt-2 pl-2 flex-grow flex flex-col ">
-                <h2 className="font-bold text-2xl pb-2">{book.title}</h2>
-                <p className="text-gray-700 text-xl pb-2"> <strong>Автор:</strong> {book.author}</p>
-                <p className="text-gray-700 text-xl pb-2"><strong>Жанр:</strong> {book.genre}</p>
-                <p className="text-gray-700 text-xl pb-2"><strong>Дата написания:</strong> {book.release_date}</p>
-                <p className="text-gray-700 text-xl pb-2">{book.description}</p>
+            <div
+                className="absolute top-1 right-1 w-3 h-3 border-2 border-amber-400 text-amber-400 flex items-center justify-center text-xs">•
+            </div>
+            <div className="relative border-2 border-amber-400 p-4">
+                <div className="flex">
+                    <div className="flex-shrink-0">
+                        <picture>
+                            <img
+                                className="w-[250px] h-auto"
+                                src={book.cover}
+                                alt={book.title}
+                            />
+                        </picture>
+                    </div>
+                    <div className="pl-4 flex-grow flex flex-col">
+                        <h2 className="font-bold text-2xl pb-2">{book.title}</h2>
+                        <p className="text-gray-700 text-xl pb-2"><strong>Автор:</strong> {book.author}</p>
+                        <p className="text-gray-700 text-xl pb-2"><strong>Жанр:</strong> {book.genre}</p>
+                        <p className="text-gray-700 text-xl pb-2"><strong>Дата написания:</strong> {book.release_date}
+                        </p>
+                        <p className="text-gray-700 text-xl pb-2">{book.description}</p>
+                    </div>
+                </div>
+                <div
+                    className="absolute bottom-[-1px] left-[-1px] w-3 h-3 border-2 border-amber-400  text-amber-400 flex items-center justify-center">•
+                </div>
+                <div
+                    className="absolute bottom-[-1px] right-[-1px] w-3 h-3 border-2 border-amber-400 text-amber-400 flex items-center justify-center ">•
+                </div>
             </div>
         </div>
     )
