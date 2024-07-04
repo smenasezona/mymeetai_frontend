cff03ddf0da24290be5eaaa18e10b524273ce2bb
smenasezona <artemkiasko@gmail.com>
2024-07-04 18:44:30 +0300

    add pagination styles

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useEffect} from "react";
 
 interface PaginationProps {
     currentPage: number;
@@ -6,19 +7,28 @@ interface PaginationProps {
     onPageChange: (page: number) => void;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({currentPage, totalPages, onPageChange}) => {
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        })
+    }, [currentPage])
+
     return (
         <div className="flex justify-center items-center gap-10 pb-6">
             <button
                 disabled={currentPage === 1}
                 onClick={() => onPageChange(currentPage - 1)}
-                className="cursor-pointer bg-amber-400 text-white font-bold py-2 px-4 rounded-l">
+                className={`cursor-pointer border-2 border-amber-400 py-2 px-4 hover:bg-amber-400 transition duration-300 rounded-tl-md rounded-bl-md ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ''}`}>
                 Назад
             </button>
             <button
                 disabled={currentPage === totalPages}
                 onClick={() => onPageChange(currentPage + 1)}
-                className="cursor-pointer bg-amber-400 text-white font-bold py-2 px-4 rounded-l">
+                className={`cursor-pointer border-2 border-amber-400 py-2 px-4 hover:bg-amber-400 transition duration-300 rounded-tr-md rounded-br-md ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : ''}`}>
                 Вперед
             </button>
         </div>
